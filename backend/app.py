9720b23d3514c6599289e9f0d40060aec0dcb78e
mrx_fr1ends <prosto.pasha123@gmail.com>
2023-07-07 16:28:11 +0300

    First commit, added some backend

---
+++ backend/app.py
@@ -0,0 +1,96 @@
+from flask import Flask, request, session
+from flask_socketio import SocketIO, join_room as join_room_socket
+from uuid import uuid4 as v4
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5a23906e-f62b-4110-8542-eb872e6a3316'
+
+socketio = SocketIO(app)
+
+rooms = {}
+
+def create_game():
+    return {"game": 123}
+
+def create_card():
+    # TODO: создать карточку на основе настроек
+    return {"test": '123'}
+
+def create_player(name, card, is_host):
+    # TODO: сделать создание игрока
+    return {
+        "id": str(v4()),
+        "name": name,
+        "is_host": is_host,
+        **card
+    }
+
+def create_room_(name, creator, game):
+    # TODO: сделать создание комнаты
+    # TODO: добавить создание и генерацию игры с выбранными настройками
+    return {
+        "id": str(v4()), 
+        "name": name, 
+        "players": [creator],
+        "game": game
+    }
+
+@app.route('/room', methods=['POST'])
+def create_room():
+    req = request.get_json()
+    print(req)
+    print(rooms)
+
+    room_name, player_name = req['room_name'], req['player_name']
+
+    # ? Подумать над добавлением настроек
+    # TODO: проверка на допустимость создания комнаты с таким названием
+    if room_name in rooms:
+        return {"error_message": "Комната с таким названием уже существует"}, 403
+
+    card = create_card()
+    # TODO: добавление игрока в базу данных
+    creator = create_player(player_name, card, True)
+    game = create_game()
+    # TODO: добавление комнаты в базу данных
+    rooms[room_name] = create_room_(player_name, creator, game)
+
+    session['player_name'] = player_name
+    session['room_name'] = room_name
+
+    return rooms[room_name], 200
+
+@app.route('/room/<name>', methods=['POST'])
+def join_room(name):
+
+    print(name)
+    # TODO: сделать проверку на существование комнаты с таким именем
+    if name not in rooms:
+        return {"error_message": "Комната с таким названием не найдена"}, 403
+    
+    req = request.get_json()
+    print(req)
+    player_name = req['player_name']
+
+    # TODO: получить комнату по name
+    room = rooms[name]
+
+    # TODO: сделать проверку на наличие игрока в этой комнате с таким же именем
+    for player in room['players']:
+        if player_name == player['name']:
+            return {"error_message": "В комнате уже есть игрок с таким же именем"}, 403
+
+    card = create_card()
+    # TODO: добавление игрока в базу данных
+    player = create_player(player_name, card, False)
+    # TODO: добавить в таблицу с игроками нового игрока с генерированной карточки
+    rooms[name]['players'].append(player)
+
+    session['player_name'] = player_name
+    session['room_name'] = room["name"]
+
+    print(rooms)
+    return room, 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
